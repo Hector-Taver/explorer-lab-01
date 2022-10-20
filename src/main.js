e764831e558b3ac744c522491afd4416ffbe2e35
Hector van Tol Taver <hectorvttaver@gmail.com>
2022-10-19 22:51:12 -0300

    Aula 2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import IMask from "imask";
+
 import "./css/index.css";
 
 const creditCardBgColor01 = document.querySelector(
@@ -17,7 +19,65 @@ const setCardType = (type) => {
 
   creditCardBgColor01.setAttribute("fill", colors[type][0]);
   creditCardBgColor02.setAttribute("fill", colors[type][1]);
-  creditCardLogo.setAttribute("src", `cc-${type}.svg`)
-}
+  creditCardLogo.setAttribute("src", `cc-${type}.svg`);
+};
 
-globalThis.setCardType = setCardType
+globalThis.setCardType = setCardType;
+
+const securityCode = document.querySelector("#security-code");
+const securityCodePattern = {
+  mask: "0000",
+};
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+const expirationDate = document.querySelector("#expiration-date");
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+};
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
+
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(5[1-5]\d{0,2}|22[2-9]\d|2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: (appended, dynamicMasked) => {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+
+    const foundMask = dynamicMasked.compiledMasks.find(({ regex }) => {
+      return number.match(regex);
+    });
+
+    return foundMask
+  },
+};
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
